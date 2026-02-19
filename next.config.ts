1b6ab38444b439ad10ba0bb2cb72993f478e2a80
Ethan Della Posta <ethanjdellaposta@gmail.com>
2026-02-18 19:49:51 -0500

    config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "solarsys-app.vercel.app",
+          },
+        ],
+        destination: "https://solarsystemsim.ethandellaposta.dev/:path*",
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
